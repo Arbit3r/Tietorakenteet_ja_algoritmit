194847c04417915688df9bdd26ac527c27a0cc4b
Arbit3r <rikukoski2@gmail.com>
2022-12-17 18:43:13 +0200

    Korjauksia

--- Harjoitus17/src/src/Menu.java
+++ Harjoitus17/src/src/Menu.java
@@ -5,6 +5,8 @@
 
 package src;
 
+import java.util.TreeSet;
+
 /**
  *
  * @author kamaj
@@ -21,55 +23,27 @@ public class Menu {
 //printMenu alkaa------------------------------------------------------------------
 	private static void printMenu() {
 		char select;
-		BinaryTree tree = new BinaryTree();
-		String data;
+		TreeSet<Integer> ts = new TreeSet<Integer>();
+		int data;
 		do {
 
 			System.out.println("\n\t\t\t1. Lisää avain.");
 			System.out.println("\t\t\t2. Etsi avaimella.");
-			System.out.println("\t\t\t3. Käy puu läpi esijärjestyksessä.");
-			System.out.println("\t\t\t4. Poisto ");
-			System.out.println("\t\t\t5. Korkeus ");
-			System.out.println("\t\t\t6. lopetus ");
+			System.out.println("\t\t\t3. Poisto ");
 			System.out.print("\n\n"); // tehdään tyhjiä rivejä
 			select = Lue.merkki();
 			switch (select) {
 			case '1':
 				System.out.println("Anna uusi avain (merkkijono)");
-				data = new String(Lue.rivi());
-				tree.insert(data);
+				data = Integer.parseInt(Lue.rivi());
+				ts.add(data);
 				break;
 			case '2':
 				System.out.println("Anna etsittävä avain (merkkijono)");
-				data = Lue.rivi();
-				if (tree.find(data) != null) {
-					System.out.println("Avain löytyi.");
-				} else
-					System.out.println("Avainta ei löytynyt.");
-
+				data = Integer.parseInt(Lue.rivi());
+				ts.contains(data);
 				break;
 			case '3':
-				tree.preOrder();
-				char h = Lue.merkki(); // pysäytetään kontrolli
-				break;
-
-			case '4':
-				System.out.println("Anna poistettava avain (merkkijono)");
-				data = Lue.rivi();
-				tree.delete(data);
-				tree.preOrder();
-				break;
-			case '5':
-				System.out.println("Anna etsittävä avain (merkkijono)");
-				data = Lue.rivi();
-				try {
-					BinaryTree bt = tree.find(data);
-					System.out.println(tree.heightCalc(bt.getRoot()));
-				} catch (Exception e) {
-					// TODO: handle exception
-				}
-				break;
-			case '6':
 				break;
 			}
 		} while (select != '6');
